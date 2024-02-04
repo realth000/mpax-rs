77a9006da33475c6692c48b8282d84fcd7144b54
realth000 <a598919335@gmail.com>
2024-02-04 16:12:08 +0800

    Init repo

---
+++ mpaxctl/src/main.rs
@@ -0,0 +1,6 @@
+use libmpax::add;
+
+fn main() {
+    let x = add(1, 2);
+    println!("Hello, world! {x}");
+}
---
+++ mpaxd/src/main.rs
@@ -0,0 +1,6 @@
+use libmpax::add;
+
+fn main() {
+    let x = add(2, 3);
+    println!("Hello, world! {x}");
+}
