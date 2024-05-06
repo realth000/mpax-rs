35b393cc863a50bf916a2b75da630a1f99909f7b
realth000 <a598919335@gmail.com>
2024-05-06 20:36:43 +0800

    feat(ctl): Some code in cli

--- mpaxctl/src/main.rs
+++ mpaxctl/src/main.rs
@@ -1,4 +1,20 @@
+use std::net::Ipv4Addr;
+
+use reqwest::Client;
+use reqwest::redirect::Policy;
+
 use libmpax::add;
+
+mod config;
+
+/// Build a http client instance.
+fn build_net_client() -> Client {
+    return Client::builder()
+        .redirect(Policy::none())
+        .local_address(Ipv4Addr::new(127, 0, 0, 1).into())
+        .build()
+        .unwrap();
+}
 
 fn main() {
     let x = add(1, 2);
